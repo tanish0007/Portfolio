92a172f574a1b460f42edb906cb1aa2b675e4f52
tanish0007 <jangratanish0007@gmail.com>
2026-01-08 18:43:55 +0530

    Navbar completed

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 const navLinks = [
     { href:  "#about", label: "About" },
@@ -9,6 +11,9 @@ const navLinks = [
 ]
 
 export const Navbar = () => {
+
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     return (<header className="fixed top-0 left-0 right-0 bg-transparent py-5">
         <nav className="container mx-auto px-6 flex items-center justify-between">
             <a href="#" className="text-xl font-bold tracking-tight text-primary">{"<Tanish Jangra />"}</a>
@@ -32,9 +37,29 @@ export const Navbar = () => {
             </div>
 
             {/* Mobile Menu Button */}
-            <button className="md:hidden p-2 text-foreground">
-
+            <button className="md:hidden p-2 text-foreground cursor-pointer" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+                { isMobileMenuOpen ? <X size={25} /> : <Menu size={25} />}
             </button>
         </nav>
+
+        {/* Mobile Menu */}
+            { isMobileMenuOpen && (
+                <div className="md:hidden glass-strong animate-fade-in">
+                    <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                        {
+                            navLinks.map((link, index) => {
+                                return (<a 
+                                    href={link.href} 
+                                    key={index} 
+                                    className="text-lg text-muted-foreground hover:text-foreground py-2"
+                                    >
+                                    {link.label}
+                                </a>)
+                            }) 
+                        }
+                        <Button size="sm">Contact Me</Button>
+                    </div>
+            </div>
+            )}
     </header>)
 }
